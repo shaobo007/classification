da4bf2d28c637028294557c4ead2216b2b2c9d9c
shaobo-ubuntu <zsb940680883@163.com>
2021-11-25 20:58:23 +0800

    transformer model

--- models.py
+++ models.py
@@ -1,5 +1,9 @@
+from tkinter.constants import NO
 import torch
+import math
 from torch import nn
+from torch._C import dtype
+from torch.nn.modules import dropout
 from torch.nn.modules.linear import Linear
 from torch.nn.modules.pooling import MaxPool2d
 import torchvision
@@ -144,7 +148,7 @@ class ResNet(nn.Module):
 '''定义一个vision transformer'''
 
 
-class Pic_Embedding(nn.Module):  #
+class Pic_Embedding(nn.Module):  # 将图片转化为token做embedding
     def __init__(self, **kwargs):
         super(Pic_Embedding).__init__(**kwargs)
 
@@ -153,11 +157,18 @@ class Pic_Embedding(nn.Module):  #
 
 
 class Position_Encoding(nn.Module):  # 位置编码
-    def __init__(self, **kwargs):
+    def __init__(self, num_hiddens, dropout, max_len=1000, **kwargs):
         super(Position_Encoding).__init__(**kwargs)
+        self.dropout = nn.Dropout(dropout)
+        self.P = torch.zeros((1, max_len, num_hiddens))
+        X = torch.arrange(max_len, dtype=torch.float32).reshape(-1, 1) / torch.pow(
+            10000, torch.arrange(0, num_hiddens, 2, dtype=torch.float32) / num_hiddens)
+        self.P[:, :, 0::2] = torch.sin(X)  # 从第0个开始，间隔为2
+        self.P[:, :, 1::2] = torch.cos(X)  # 从第一个开始，间隔为2
 
     def forward(self, x):
-        pass
+        x = x + self.P[:, :x.shape[1], :].to(x.device)
+        return self.dropout(x)
 
 
 class PositionWiseFFN(nn.Module):  # Feed-Forward neural network
@@ -174,11 +185,84 @@ class PositionWiseFFN(nn.Module):  # Feed-Forward neural network
 
 
 class AddNorm(nn.Module):
-    def __init__(self, **kwargs):
+    def __init__(self, normalized_shape, dropout, **kwargs):
         super(AddNorm).__init__(**kwargs)
+        self.dropout = nn.Dropout(dropout)
+        self.ln = nn.LayerNorm(normalized_shape)
 
     def forward(self, x, y):
-        pass
+        return self.ln(self.dropout(y) + x)
+
+
+def sequence_mask(X, valid_len, value=0):
+    """Mask irrelevant entries in sequences."""
+    maxlen = X.size(1)
+    mask = torch.arange((maxlen), dtype=torch.float32,
+                        device=X.device)[None, :] < valid_len[:, None]
+    X[~mask] = value
+    return X
+
+
+def masked_softmax(X, valid_lens):
+    if valid_lens is not None:
+        return nn.functional.softmax(X, dim=-1)
+    else:
+        shape = X.shape
+        if valid_lens.dim() == 1:
+            valid_lens = torch.repeat_interleave(valid_lens, shape[1])
+        else:
+            valid_lens = valid_lens.reshape(-1)
+
+        X = sequence_mask(X.reshape(-1, shape[-1]), valid_lens, value=-1e6)
+        return nn.functional.softmax(X.reshape(shape), dim=-1)
+
+
+class DotProductAttention(nn.Module):  # q, k, v做点积
+    def __init__(self, dropout, **kwargs):
+        super(DotProductAttention).__init__(**kwargs)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, q, k, v, valid_lens=None):
+        d = q.shape[-1]
+        scores = torch.bmm(q, k.transpose(1, 2)) / math.sqrt(d)
+        self.attention_weights = masked_softmax(scores, valid_lens)
+        return torch.bmm(self.dropout(self.attention_weights), v)
+
+
+def transpose_qkv(X, num_heads):
+    X = X.reshape(X.shape[0], X.shape[1], num_heads, -1)
+    X = X.permute(0, 2, 1, 3)
+    return X.reshape(-1, X.shape[2], X.shape[3])
+
+
+def transpose_output(X, num_heads):
+    X = X.reshape(-1, num_heads, X.shape[1], X.shape[2])
+    X = X.permute(0, 2, 1, 3)
+    return X.reshape(X.shape[0], X.shape[1], -1)
+
+
+class MuiltiHead_Attention(nn.Module):  # 多头注意力机制实现
+    def __init__(self, key_size, query_size, value_size,
+                 num_hiddens, num_heads, dropout, bias=False, **kwargs):
+        super(MuiltiHead_Attention).__init__(**kwargs)
+        self.num_heads = num_heads
+        self.attention = DotProductAttention(dropout)
+        self.W_q = nn.Linear(query_size, num_hiddens, bias=bias)
+        self.W_k = nn.Linear(key_size, num_hiddens, bias=bias)
+        self.W_v = nn.Linear(value_size, num_hiddens, bias=bias)
+        self.W_o = nn.Linear(num_hiddens, num_hiddens, bias=bias)
+
+    def forward(self, queries, keys, values, valid_lens):
+        queries = transpose_qkv(self.W_q(queries), self.num_heads)
+        keys = transpose_qkv(self.W_q(keys), self.num_heads)
+        values = transpose_qkv(self.W_q(values), self.num_heads)
+        if valid_lens is not None:
+            valid_lens = torch.repeat_interleave(
+                valid_lens, repeats=self.num_heads, dim=0)
+
+        output = self.attention(queries, keys, values, valid_lens)
+        output_concat = transpose_output(output, self.num_heads)
+        return self.W_o(output_concat)
 
 
 class EncoderBlk(nn.Module):
